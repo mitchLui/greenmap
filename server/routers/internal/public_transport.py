3661e5a788e5075c6cbfe5a2268c9aa1a92efbb0
Mitch Lui <mitch@mitchlui.dev>
2022-02-26 19:24:11 +0000

    lon

--- server/routers/internal/public_transport.py
+++ server/routers/internal/public_transport.py
@@ -3,7 +3,6 @@ import requests
 
 PLACES_URL: str = "https://transportapi.com/v3/uk/places.json?"
 
-
 class PublicTransportService(Service):
 
     def get_stations(self, lat: float, lng: float, transport_type: str):
@@ -11,7 +10,7 @@ class PublicTransportService(Service):
         params["app_id"] = self.app_id
         params["app_key"] = self.app_key
         params["lat"] = str(lat)
-        params["long"] = str(lng)
+        params["lon"] = str(lng)
         params["type"] = "train_station" if transport_type == "train" else "bus_stop"
         r = requests.get(PLACES_URL, params)
         if r.status_code == 200:
@@ -31,12 +30,12 @@ class PublicTransportService(Service):
             station: dict = dict()
             station["name"] = entry["name"]
             station["lat"] = entry["latitude"]
-            station["lon"] = entry["longitude"]
-            station["distance"] = entry["distance"]
+            station["long"] = entry["longitude"]
+            station["distance"] = entry.get("distance", "unknown")
             stations.append(station)
         return stations
 
 
 if __name__ == "__main__":
     public_transport = PublicTransportService("TRANSPORTAPIAPPID", "TRANSPORTAPIAPPKEY")
-    print(public_transport.get_stations(51.455795, -2.602835, "train"))
+    print(public_transport.get_train_stations(51.455795, -2.602835))
