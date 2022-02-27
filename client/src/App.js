44d92eed7e7ee06af0a8d91381744dcb3a35c571
Salman Khan <salman@msalmankhan.co.uk>
2022-02-27 07:13:42 +0000

    blah

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
         const id = navigator.geolocation.watchPosition(
             (pos) => {
                 setCoords(pos.coords.latitude, pos.coords.longitude);
-                if(centre.length === 0) {
+                if (centre.length === 0) {
                     setCentre([pos.coords.latitude, pos.coords.longitude])
                 }
             },
@@ -34,7 +34,6 @@ function App() {
         );
 
 
-
         return () => {
             navigator.geolocation.clearWatch(id);
         }
@@ -44,13 +43,51 @@ function App() {
     return (
         <div className="App">
             <Navbar/>
-            <Mapbox searchBarVisibility={searchBarVisibility} setSearchBarVisibility={setSearchBarVisibility} lat={lat} setLat={setLat} lng={lng} setLng={lng} centre={centre} setCentre={setCentre}/>
+            <Mapbox searchBarVisibility={searchBarVisibility} setSearchBarVisibility={setSearchBarVisibility} lat={lat}
+                    setLat={setLat} lng={lng} setLng={lng} centre={centre} setCentre={setCentre}/>
+            <Clock />
+
             {
                 searchBarVisibility &&
-                <Search searchBarVisibility={searchBarVisibility} setSearchBarVisibility={setSearchBarVisibility} lat={lat} lng={lng} />
+                <Search searchBarVisibility={searchBarVisibility} setSearchBarVisibility={setSearchBarVisibility}
+                        lat={lat} lng={lng}/>
             }
         </div>
     );
+}
+
+const Clock = () => {
+    const [time, setTime] = useState("00:00");
+    const [amPm, setAmPm] = useState("AM");
+    const [date, setDate] = useState("27th February 2020");
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Decemmber"];
+    useEffect(() => {
+        const id = setTimeout(() => {
+            const date = new Date(Date.now());
+            let hour = date.getHours(),
+                am = true,
+                minute = date.getMinutes();
+
+            if (hour > 12) {
+                am = false;
+                hour -= 12;
+            }
+
+            setTime(`${String(hour).padStart(2, 0)}:${String(minute).padStart(2, 0)}`);
+            setAmPm(am ? "AM" : "PM");
+            setDate(`${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`);
+        }, 1000)
+    })
+
+    return(
+        <div className={"clock"}>
+            <div className={"time"}>
+                <span className={"big"}>{time}</span> {amPm}
+            </div>
+            <div className={"date"}>{date}</div>
+        </div>
+    )
+
 }
 
 export default App;
