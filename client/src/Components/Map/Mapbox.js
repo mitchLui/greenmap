eaeac9dcae3c8fccfba29cab2998099bb53638cf
Salman Khan <salman@msalmankhan.co.uk>
2022-02-27 01:14:15 +0000

    fuck mapbox even though it's sexy

--- client/src/Components/Map/Mapbox.js
+++ client/src/Components/Map/Mapbox.js
@@ -1,5 +1,5 @@
 import {useRef, useState, useEffect} from "react";
-import {Map, Marker} from 'react-map-gl';
+import {Map, Marker, ScaleControl} from 'react-map-gl';
 import "./map.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -8,9 +8,12 @@ const token = "pk.eyJ1IjoibXNhbG1hbmtoYW4iLCJhIjoiY2wwNDg3aXJiMGIyYzNpb2czMWxkd2
 export const Mapbox = () => {
     const [lng, setLng] = useState(0.0);
     const [lat, setLat] = useState(0.0);
-    const [zoom, setZoom] = useState(9);
+    const [centre, setCentre] = useState([]);
+    const [zoom, setZoom] = useState(15);
     const [height, setHeight] = useState(document.documentElement.clientHeight);
     const [width, setWidth] = useState(document.documentElement.clientWidth);
+    const [dragStart, setDragStart] = useState([0, 0])
+
 
     useEffect(() => {
         const handleResize = () => {
@@ -26,14 +29,15 @@ export const Mapbox = () => {
     const setCoords = (lat, lng) => {
         setLng(lng);
         setLat(lat);
-
-        console.log(lat, lng)
     }
 
     useEffect(() => {
         const id = navigator.geolocation.watchPosition(
             (pos) => {
                 setCoords(pos.coords.latitude, pos.coords.longitude);
+                if(centre.length === 0) {
+                    setCentre([pos.coords.latitude, pos.coords.longitude])
+                }
             },
             (err) => {
                 console.log(err);
@@ -49,17 +53,29 @@ export const Mapbox = () => {
         }
     })
 
+    const handleWheelZoom = ({originalEvent: {deltaY}}) => {
+        setZoom(zoom - deltaY/20);
+    }
+
+    const handleDrag = (e) => {
+        setCentre([centre[1] - (dragStart[1] - e.originalEvent.screenY)/(10000*zoom), centre[0] - (dragStart[0] - e.originalEvent.screenX)/(10000*zoom)]);
+    }
+
+    console.log(centre)
 
     return <Map
-        initialViewState={{
-            longitude: lng,
-            latitude: lat,
-            zoom: zoom
-        }}
+        scrollZoom={false}
+        doubleClickZoom={false}
+        zoom={zoom}
+        latitude={centre[0]}
+        longitude={centre[1]}
         mapboxAccessToken={token}
         className={"map-container"}
         style={{width, height}}
         mapStyle="mapbox://styles/mapbox/dark-v10"
+        onWheel={handleWheelZoom}
+        onDrag={handleDrag}
+        onDragStart={({originalEvent: {screenX, screenY}}) => setDragStart([screenX, screenY])}
     >
         <Marker longitude={lng} latitude={lat} color="red" />
     </Map>;
