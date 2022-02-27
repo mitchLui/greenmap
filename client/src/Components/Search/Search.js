972e23504bd4b441d16b245410ba37a0f2bf4194
Matthew Swann <cv20549@bristol.ac.uk>
2022-02-27 11:45:41 +0000

    stuff

--- client/src/Components/Search/Search.js
+++ client/src/Components/Search/Search.js
@@ -37,6 +37,8 @@ export const Search = ({searchBarVisibility, setSearchBarVisibility, lat, lng, s
                         suggestions.map((place, key) => <div className={"suggestion"} onClick={() => {
                             setCentre([place.coords[1], place.coords[0]]);
                             setSearchBarVisibility(false);
+                            fetch("https://909f-2001-630-e4-4220-55c7-d61c-6788-9101.ngrok.io/navigation/?src_long=-2.6027&src_lat=51.4545&dest_long=-2.6220&dest_lat=51.4637")
+                            .then(res => res.json()).then(j => {setInfo(j.data);});
                         }}>{place.name}</div>)
                     }
                 </div>
--- client/src/Components/SearchResult/Result.js
+++ client/src/Components/SearchResult/Result.js
@@ -1,4 +1,9 @@
 import "./Result.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBus, faP } from "@fortawesome/free-solid-svg-icons";
+import { faPersonWalking } from "@fortawesome/free-solid-svg-icons";
+import { faTrainSubway } from "@fortawesome/free-solid-svg-icons";
+
 
 export const Result = (recommendations) => {
     return <div className="result">
@@ -10,7 +15,18 @@ export const Result = (recommendations) => {
                 <div className="emissions">
                     {route.emissions + "g of CO2 produced"}
                 </div>
-                {route.legs.map(leg => leg.mode)}
+                {route.legs.map(leg => {
+                    if (leg.mode === "bus") {
+                        return <FontAwesomeIcon icon={faBus} />
+                    } else if (leg.mode === "walk") {
+                        return <FontAwesomeIcon icon={faPersonWalking} />
+                    } else if (leg.mode === "train") {
+                        return <FontAwesomeIcon icon={faTrainSubway} />
+                    } else if (leg.mode === "voi") {
+                        return <svg href="/voi-icon.svg"></svg>
+                    }
+                    return <span>{leg.mode}</span>;
+                })}
                 <br />
             </div>
         })}
