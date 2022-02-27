7a9eb480c1d860e5662e524251ceeef7e1234257
Mitch Lui <mitch@mitchlui.dev>
2022-02-27 00:58:24 +0000

    carbon offset calculator

--- server/routers/internal/carbon_offset.py
+++ server/routers/internal/carbon_offset.py
@@ -1,5 +1,5 @@
 from typing import List, Union
-import requests
+
 
 class CarbonOffsetCalculator:
 
@@ -8,27 +8,21 @@ class CarbonOffsetCalculator:
         self.carbon_emissions = {
             "scooter": 0.11,
             "bus": 0.89,
-            "train": 35.1,
+            "train": 0.035,
             "ev": 0.06
         }
-        self.recommended_sites = ""
 
 
     def calculate_carbon_offset(self, distance: float, type: str) -> float:
-        carbon_offset = 0
-        if type == "scooter":
-            carbon_offset = self.scooter_carbon * distance
-        elif type == "bus":
-            carbon_offset = self.bus_carbon * distance
-        elif type == "train":
-            carbon_offset = self.train_carbon * distance
-        elif type == "ev":
-            carbon_offset = self.ev_carbon * distance
+        if self.carbon_emissions.get(type) is not None:
+            return round(self.carbon_emissions[type] * distance, 2)
         else:
             raise Exception("Unknown type")
-        return round(carbon_offset, 2)
 
     def calculate_carbon_offset_list(self, distances: List[float], types: List[str]) -> Union[float, float]:
+        # NOTE: distances is in metres
+        # Convert back to km
+        distances = [d / 1000 for d in distances]
         carbon_offset = 0
         if len(distances) != len(types):
             raise Exception("The number of distances and types must be the same")
@@ -38,8 +32,8 @@ class CarbonOffsetCalculator:
 
 if __name__ == "__main__":
     co = CarbonOffsetCalculator()
-    print(co.calculate_carbon_offset(10, "scooter"))
-    print(co.calculate_carbon_offset(10, "bus"))
-    print(co.calculate_carbon_offset(10, "train"))
-    print(co.calculate_carbon_offset(10, "ev"))
+    print(co.calculate_carbon_offset(2000, "scooter"))
+    #print(co.calculate_carbon_offset(10, "bus"))
+    #print(co.calculate_carbon_offset(10, "train"))
+    #print(co.calculate_carbon_offset(10, "ev"))
     print(co.calculate_carbon_offset_list([10, 10, 10, 10], ["scooter", "bus", "train", "ev"]))
