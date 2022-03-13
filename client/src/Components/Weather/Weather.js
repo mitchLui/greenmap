ba38aeac4ff71423fb2014b3721d0c9a67f74ac6
Salman Khan <salman@msalmankhan.co.uk>
2022-03-13 13:06:19 +0000

    fixed weather and added env

--- client/src/Components/Weather/Weather.js
+++ client/src/Components/Weather/Weather.js
@@ -1,14 +1,42 @@
 import "./Weather.css";
+import {useEffect, useState} from "react";
 
-export const Weather = (weather) => {
-    return (<div className={"weather"}>
-                {weather.icon_url !== undefined && <div className="weather-info">
-                    <div className={"weather-icon"}>
-                        <img src={weather.icon_url} alt="weather icon"/>
-                    </div>
-                    <div className="temperature">
-                        {weather.temp}
-                    </div>
-                </div>}
-        </div>)
+export const Weather = ({lat, long}) => {
+    const [weather, setWeather] = useState(null)
+
+    useEffect(async () => {
+            const hour = 60 * 60 * 1000;
+            if (!weather || (weather.time - Date.now()) > hour) {
+                const data = await getWeatherData(lat, long);
+                setWeather({time: Date.now(), data})
+            }
+        }
+    );
+
+    if(!weather) return <></>;
+
+    return (
+        <div className={"weather"}>
+            <div className="weather-info">
+                <div className={"weather-icon"}>
+                    <img src={weather.data.icon_url} alt={weather.data.description} />
+                </div>
+                <div className="weather-temp">
+                    {weather.data.temp}&deg;C
+                </div>
+            </div>
+        </div>
+    )
 };
+
+
+const getWeatherData = (lat, long) => {
+    const params = {
+            lat,
+            long,
+        },
+        url = new URL(process.env.REACT_APP_BACKEND_URL + "weather");
+
+    url.search = new URLSearchParams(params).toString();
+    return fetch(url).then(resp => resp.json()).then(({data}) => data)
+}
