5b809bde09c03a09cf66a35c52dded5f5300f5c8
Matthew Swann <cv20549@bristol.ac.uk>
2022-03-28 18:02:22 +0100

    fixed union find

--- server/routers/internal/unionFind.py
+++ server/routers/internal/unionFind.py
@@ -1,6 +1,6 @@
-class UnionFind:
+import math
 
-    tree = {}
+class UnionFind:
 
     def __init__(self, stuff):
         self.tree = {}
@@ -11,7 +11,8 @@ class UnionFind:
         if self.tree[x] == x:
             return x, 0
         root, depth = self.find(self.tree[x])
-        return root, depth + 1
+        self.tree[x] = root
+        return root, 1
 
     def union(self, a, b):
         x, d1 = self.find(a)
@@ -30,3 +31,14 @@ class UnionFind:
             else:
                 groups[root] = [node]
         return groups.values()
+
+if __name__ == "__main__":
+    data = [1,3,4,5,6,7,8,9,12,13,14,15,17]
+    uf = UnionFind(data)
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            a = data[i]
+            b = data[j]
+            if abs(a - b) <= 1:
+                uf.union(a, b)
+    print(uf.getGroups())
